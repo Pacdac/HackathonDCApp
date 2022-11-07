123560667b07221f33be2e53951d9086e2f29ef4
François Liard <lifra05@yahoo.fr>
2022-11-07 15:21:43 +0100

    merge struct, hardcode interface, swap function

--- contract/DCApp.sol
+++ contract/DCApp.sol
@@ -1,9 +1,32 @@
-@@ -1,278 +1,283 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-import "./Struct.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+
+struct userDCAData {
+    uint periodDays;
+    uint totalOccurences;
+    uint currentOccurence;
+    uint amountPerOccurence;
+    address tokenIn;
+    address tokenOut;
+    uint tokenInLockedAmount;
+    address[] swapPath;
+    uint fee5Decimals;
+}
 
 //import pancakeswap router
 interface IPancakeRouter01 {
@@ -141,101 +164,87 @@ interface IPancakeRouter02 is IPancakeRouter01 {
     ) external;
 }
 
+interface IPancakeFactory {
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    function feeTo() external view returns (address);
+    function feeToSetter() external view returns (address);
+
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+    function allPairs(uint) external view returns (address pair);
+    function allPairsLength() external view returns (uint);
+
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+
+    function setFeeTo(address) external;
+    function setFeeToSetter(address) external;
+}
+
 //main contract
 contract DCApp{
-
+    /*
+        Optimization ideas :
+            - Remove constant variable and just directly use the values
+                Seems to save a little gas, about 20 each time
+    */
     mapping (address => mapping( uint => userDCAData)) public mapDCA;
+
     //address of the pancakeswap v2 router
     address private constant PANCAKEV2ROUTER = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
+    //address of the pancakeswap v2 factory
+    address private constant PANCAKEV2FACTORY = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
+
+    //address of the pancakeswap v2 router ON TESTNET
+    address private constant PANCAKEV2ROUTERTESTNET = 0xD99D1c33F9fC3444f8101754aBC46c52416550D1;
+    //address of the pancakeswap v2 factory ON TESTNET
+    address private constant PANCAKEV2FACTORYTESTNET = 0x6725F303b657a9451d8BA641348b6761A6CC7a17;
 
     //address of WBNB token.  This is needed because some times it is better to trade through WBNB. 
     address private constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
 
-    function swap(address _tokenIn, address _tokenOut, uint256 _amountIn, uint256 _amountOutMin, address _to) private {
-      
-        //first we need to transfer the amount in tokens from the msg.sender to this contract
-        //this contract will have the amount of in tokens
-        //IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
-    
-        //next we need to allow the uniswapv2 router to spend the token we just sent to this contract
-        //by calling IERC20 approve you allow the uniswap contract to spend the tokens in this contract 
-        IERC20(_tokenIn).approve(PANCAKEV2ROUTER, _amountIn);
+    //-------------REPLACE WBNBTestNet BY WBNB FOR THE MAIN CHAIN-----------
+    address private constant WBNBTestNet = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd;
 
-        //path is an array of addresses.
-        //this path array will have 3 addresses [tokenIn, WBNB, tokenOut]
-        //the if statement below takes into account if token in or token out is WBNB.  then the path is only 2 addresses
-        address[] memory path;
-        if (_tokenIn == WBNB || _tokenOut == WBNB) {
-          path = new address[](2);
-          path[0] = _tokenIn;
-          path[1] = _tokenOut;
-        } else {
-          path = new address[](3);
-          path[0] = _tokenIn;
-          path[1] = WBNB;
-          path[2] = _tokenOut;
-        }
-        //then we will call swapExactTokensForTokensSupportingFeeOnTransferTokens
-        //for the deadline we will pass in block.timestamp
-        //the deadline is the latest time the trade is valid for
-        IPancakeRouter02(PANCAKEV2ROUTER).swapExactTokensForTokensSupportingFeeOnTransferTokens(_amountIn, _amountOutMin, path, _to, block.timestamp);
-    }
-    
-       
-    function getAmountOutMin(address _tokenIn, address _tokenOut, uint256 _amountIn) private view returns (uint256) {
-       //this function will return the minimum amount from a swap
-       //input the 3 parameters below and it will return the minimum amount out
-       //this is needed for the swap function above
-       //path is an array of addresses.
-       //this path array will have 3 addresses [tokenIn, WBNB, tokenOut]
-       //the if statement below takes into account if token in or token out is WBNB.  then the path is only 2 addresses
-        address[] memory path;
-        if (_tokenIn == WBNB || _tokenOut == WBNB) {
-            path = new address[](2);
-            path[0] = _tokenIn;
-            path[1] = _tokenOut;
-        } else {
-            path = new address[](3);
-            path[0] = _tokenIn;
-            path[1] = WBNB;
-            path[2] = _tokenOut;
-        }
-        
-        uint256[] memory amountOutMins = IPancakeRouter02(PANCAKEV2ROUTER).getAmountsOut(_amountIn, path);
-        return amountOutMins[path.length -1];  
-    }  
+    event addedNewDCA(uint timestamp, address userAddress);
+    event occurenceExecuted(uint timestamp, address indexed userAddress, uint totalOccurence, uint currentOccurence);
+    event deletedDCA(uint timestamp, address userAddress);   
 
-    function addNewDCAToUser(uint _period, uint _totalOccurences, uint _amountPerOccurrence, address _tokenIn, address _tokenOut)
-                 external returns (uint) {
+    function addNewDCAToUser(uint _period, uint _totalOccurences, uint _amountPerOccurrence, address _tokenIn, address _tokenOut, uint _fee10Decimals)
+                 external payable {
         //Add a DCA to the calling user and lock the necessary funds on the contract
         //The funds can be unlocked at all time be cancelling the DCA
+        //If one of the two address is address(0), its BNB
         //TODO :
         // - Security checks
 
-        //Transfer the tokens to this contract
         uint timestamp = block.timestamp;
-        
-        require(_period > 0, "Error: DCA Period must be greater than 0");
+
+        require(_period * _totalOccurences * _amountPerOccurrence > 0, 
+                "Error: DCA Period, Total Occurences and Amount Per Occurence must be all greater than 0");
         require(mapDCA[msg.sender][timestamp].periodDays == 0, "Error: Retry in a few seconds");
-        
+
         //Register the DCA for this user
         mapDCA[msg.sender][timestamp] = userDCAData(_period, _totalOccurences, 0, _amountPerOccurrence, _tokenIn, _tokenOut,
-                                                    _totalOccurences * _amountPerOccurrence, 0);
+                                                    _totalOccurences * _amountPerOccurrence, buildSwapPath(_tokenIn, _tokenOut), _fee10Decimals);
 
-        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _totalOccurences * _amountPerOccurrence);
-        
-        
+        if (_tokenIn != address(0)) {
+            //Require the user to approve the transfer beforehand
+            IERC20(_tokenIn).transferFrom(msg.sender, address(this), _totalOccurences * _amountPerOccurrence);
+        }
+        else {
+            require(msg.value >= _totalOccurences * _amountPerOccurrence, "Error: You did not sent enough BNB");
+        }
 
-        return timestamp;
+        emit addedNewDCA(timestamp, msg.sender);
+
+        executeSingleUserDCA(msg.sender, timestamp);
     }
 
-    function deleteUserDCA(uint _startDate) public {
-        /*Delete a DCA to free the storage and earn back gas
-            Transfer back to the user the remaining amount 
+    function deleteUserDCA(uint _startDate) external {
+        /*Delete a DCA to free the storage and earn back gas and transfer back to the user the remaining amount 
+            address(0) as the IN our OUT token mean that it is BNB
             TODO : 
              - Security checks
-             - Lock Data (all the DCA of this user) while being modified
-             - Transfer the funds back to the user
             Should be deletable by :
                 -The user at any time
         */ 
@@ -243,42 +252,121 @@ contract DCApp{
         userDCAData memory userDCA = mapDCA[msg.sender][_startDate];
         require(userDCA.periodDays > 0, "Error: This DCA does not exist");
         delete mapDCA[msg.sender][_startDate];
-        IERC20(userDCA.tokenIn).transfer(msg.sender, userDCA.tokenInLockedAmount);
-        IERC20(userDCA.tokenOut).transfer(msg.sender, userDCA.tokenOutLockedAmount);
 
+        if (userDCA.tokenIn != address(0)) {
+            IERC20(userDCA.tokenIn).transfer(msg.sender, userDCA.tokenInLockedAmount);
+        }
+        else {
+            (bool success, ) =msg.sender.call{value: userDCA.tokenInLockedAmount}("");
+            require(success, "BNB Transfer failed.");
+        }
+
+        emit deletedDCA(_startDate, msg.sender);
     }
 
     function executeSingleUserDCA(address user, uint DCAStartDate ) public {
 
-        /* Execute a single occurence of a single DCA for the user
-            Find the right one by the startDate value, which should be unique for each DCA of this user
+        /*  Execute a single occurence of a single DCA for the user
+            Find the right one by the user address and startDate value, which should be unique for each DCA of this user
             It implies that we must know this exact value i.e. querying it after creating the DCA, and stocking it on our servers
-            Would mainly be called by our server with web3.js, so the user does not have to worry about it
-
+            This function should be call by our server every automatically, depending on the period of the DCA
             Steps :
              - Security checks
-               - The msg.sender address need to be ours (?)
-               - The current date must be around startDate + period * currentOccurence +- margin (maybe a margin of about an hour or so)
-               - The currentOccurence number should be lower than the totalOccurence 
+               - The DCA must exist
+               - The current date must be around startDate + period * currentOccurence - margin 
+               - The currentOccurence number should be lower than the totalOccurence (i.e. not completed)
              - Modify the userDCAData state to reflect the operation (need to do it 1st to avoir re-entrency attacks)
-             - Swap the defined amount of tokens with the swap() function
+                - Increment the number of occurence
+                - Modify the lockedAmount of each token
+             - Swap the defined amount of tokens, minus the fee, with the swap() function 
+             - Send the fee to the address that executed this function
+             TODO: 
         */
         userDCAData memory userDCA = mapDCA[user][DCAStartDate];
 
         require(userDCA.periodDays > 0, "Error: This DCA does not exist");
         require(userDCA.totalOccurences > userDCA.currentOccurence, "Error: DCA has already been completed, the user has to retrieve its tokens");
-        require(block.timestamp > DCAStartDate + userDCA.periodDays * 86400 * (userDCA.currentOccurence + 1) - 1800, "Error: Too soon to execute this DCA");
+        require(block.timestamp > DCAStartDate + userDCA.periodDays * /*86400*/ 1  * (userDCA.currentOccurence + 1) - 1800,
+                                                                                     "Error: Too soon to execute this DCA");
 
         mapDCA[user][DCAStartDate].currentOccurence = userDCA.currentOccurence + 1;
 
-        uint amountOut = getAmountOutMin(userDCA.tokenIn, userDCA.tokenOut, userDCA.amountPerOccurence);
+        uint amountOut = IPancakeRouter02(PANCAKEV2ROUTERTESTNET)
+                            .getAmountsOut(userDCA.amountPerOccurence, userDCA.swapPath)[userDCA.swapPath.length - 1];
 
         mapDCA[user][DCAStartDate].tokenInLockedAmount = userDCA.tokenInLockedAmount - userDCA.amountPerOccurence;
-        mapDCA[user][DCAStartDate].tokenOutLockedAmount = userDCA.tokenOutLockedAmount + amountOut;
 
-        swap(userDCA.tokenIn, userDCA.tokenOut, userDCA.amountPerOccurence, amountOut, address(this));
+        uint amountWithoutFees = ((userDCA.amountPerOccurence * 100000) * (100000 - userDCA.fee5Decimals)) / 10000000000;
+        swap(userDCA.tokenIn, userDCA.tokenOut, userDCA.swapPath, amountWithoutFees, amountOut, user);
 
-       
+        IERC20(userDCA.tokenIn).transfer(msg.sender, userDCA.amountPerOccurence - amountWithoutFees);
+        emit occurenceExecuted(DCAStartDate, user, userDCA.totalOccurences, userDCA.currentOccurence + 1);
     }   
 
+    function swap(address _tokenIn, address _tokenOut, address[] memory path, uint256 _amountIn, uint256 _amountOutMin, address userAddress) private {
+        /*
+            Swap _tokenIn for _tokenOut using the PancakeSwap RouterV2 contract
+            If _tokenIn is an ERC20 it first gives _amountIn allowance to the router to do the swap
+            Then it calls the appropriate function for the swap
+            The called PancakeSwap function depends on the nature of _tokenIn and _tokenOut, if one of them is BNB or not
+        */
+
+        //Case 1: Both tokens are ERC20 tokens
+        if (_tokenIn != address(0) && _tokenOut != address(0)) {
+            IERC20(_tokenIn).approve(PANCAKEV2ROUTERTESTNET, _amountIn);
+            IPancakeRouter02(PANCAKEV2ROUTERTESTNET).swapExactTokensForTokensSupportingFeeOnTransferTokens
+                                                        (_amountIn, _amountOutMin, path, userAddress, block.timestamp);
+        }
+        //Case 2: _tokenIn is BNB
+        else if (_tokenIn == address(0)) {
+            IPancakeRouter02(PANCAKEV2ROUTERTESTNET).swapExactETHForTokensSupportingFeeOnTransferTokens{value: _amountIn}
+                                                        (_amountOutMin, path,  userAddress, block.timestamp);
+        }
+        //Case 3: _tokenOut is BNB
+        else if (_tokenOut == address(0)) {
+            IERC20(_tokenIn).approve(PANCAKEV2ROUTERTESTNET, _amountIn);
+            IPancakeRouter02(PANCAKEV2ROUTERTESTNET).swapExactTokensForETHSupportingFeeOnTransferTokens
+                                                        (_amountIn, _amountOutMin, path,  userAddress, block.timestamp);
+        }
+    }
+
+    function buildSwapPath(address _tokenIn, address _tokenOut) private pure returns (address[] memory){
+        /*
+            Check if the swap is possible on PancakeSwap and return the swap path if it is
+            If the IN or OUT token is BNB, its part of the path is set to the WBNB address because
+                it is how PancakeSwap swap functions work
+            Otherwise the swap path go throught WBNB as it seems to be cheaper (?)
+            Might not be interesting to check if the pair currently exist ? Would lower gas fee and does not
+            block the user from retrieving their tokens --- DONE ---
+        */
+        address[] memory path;
+        
+        //Case 1 : The IN token is BNB
+        if (_tokenIn == address(0)) {
+            path = new address[](2);
+            path[0] = WBNBTestNet;
+            path[1] = _tokenOut;
+        }
+        //Case 2 : The OUT token is BNB
+        else if (_tokenOut == address(0)) {
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = WBNBTestNet;
+        }
+        //Case 3 : Both token are ERC20 but swapping throught WBNB is possible
+        else if (_tokenIn == WBNBTestNet || _tokenOut == WBNBTestNet) {
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
+        } else {
+            path = new address[](3);
+            path[0] = _tokenIn;
+            path[1] = WBNBTestNet;
+            path[2] = _tokenOut;
+        }          
+        
+
+
+        return path;
+    }
 }
