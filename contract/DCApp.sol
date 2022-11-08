4e5e900eda8d5c2c30b8156caf187d92e42de174
François Liard <lifra05@yahoo.fr>
2022-11-08 13:18:15 +0100

    global variable, fee, bug fix

--- contract/DCApp.sol
+++ contract/DCApp.sol
@@ -184,33 +184,46 @@ interface IPancakeFactory {
 contract DCApp{
     /*
         Optimization ideas :
-            - Remove constant variable and just directly use the values
-                Seems to save a little gas, about 20 each time
+            - 
     */
+    address private owner;
+    address private exchangeRouterAddress;
+    address private exchangeFactoryAddress;
+    /*
+        address of the pancakeswap v2 router : 0x10ED43C718714eb63d5aA57B78B54704E256024E
+        address of the pancakeswap v2 factory : 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73
+        address of the pancakeswap v2 router ON TESTNET : 0xD99D1c33F9fC3444f8101754aBC46c52416550D1
+        address of the pancakeswap v2 factory ON TESTNET : 0x6725F303b657a9451d8BA641348b6761A6CC7a17
+        0x0000000000000000000000000000000000000000
+        0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee
+    */
+    
+    constructor (address _exchangeRouterAddress, address _exchangeFactoryAddress)  {
+       owner = msg.sender;
+       exchangeRouterAddress = _exchangeRouterAddress;
+       exchangeFactoryAddress = _exchangeFactoryAddress;
+    }
+
+    modifier onlyOwner {
+      require(msg.sender == owner, "Error: Require to be the contract's owner");
+      _;
+   }
+
     mapping (address => mapping( uint => userDCAData)) public mapDCA;
-
-    //address of the pancakeswap v2 router
-    address private constant PANCAKEV2ROUTER = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
-    //address of the pancakeswap v2 factory
-    address private constant PANCAKEV2FACTORY = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
-
-    //address of the pancakeswap v2 router ON TESTNET
-    address private constant PANCAKEV2ROUTERTESTNET = 0xD99D1c33F9fC3444f8101754aBC46c52416550D1;
-    //address of the pancakeswap v2 factory ON TESTNET
-    address private constant PANCAKEV2FACTORYTESTNET = 0x6725F303b657a9451d8BA641348b6761A6CC7a17;
 
     //address of WBNB token.  This is needed because some times it is better to trade through WBNB. 
     address private constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
 
-    //-------------REPLACE WBNBTestNet BY WBNB FOR THE MAIN CHAIN-----------
-    address private constant WBNBTestNet = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd;
+    //-------------REPLACE WBNBTESTNET BY WBNB FOR THE MAIN CHAIN-----------
+    address private constant WBNBTESTNET = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd;
 
-    event addedNewDCA(uint timestamp, address userAddress);
-    event occurenceExecuted(uint timestamp, address indexed userAddress, uint totalOccurence, uint currentOccurence);
-    event deletedDCA(uint timestamp, address userAddress);   
+    event AddedNewDCA(uint DCACreationTimestamp, address userAddress);
+    event OccurenceExecuted(uint DCACreationTimestamp, address indexed userAddress, uint totalOccurence, 
+                                uint currentOccurence, uint occurrenceTimestamp, uint nextOccurrenceTimestamp, uint estimatedMinimumAmountOut);
+    event DeletedDCA(uint DCACreationTimestamp, address userAddress);   
 
-    function addNewDCAToUser(uint _period, uint _totalOccurences, uint _amountPerOccurrence, address _tokenIn, address _tokenOut, uint _fee10Decimals)
-                 external payable {
+    function addNewDCAToUser(uint _period, uint _totalOccurences, uint _amountPerOccurrence, address _tokenIn, address _tokenOut, uint _fee5Decimals)
+                 external payable returns (uint) {
         //Add a DCA to the calling user and lock the necessary funds on the contract
         //The funds can be unlocked at all time be cancelling the DCA
         //If one of the two address is address(0), its BNB
@@ -222,10 +235,12 @@ contract DCApp{
         require(_period * _totalOccurences * _amountPerOccurrence > 0, 
                 "Error: DCA Period, Total Occurences and Amount Per Occurence must be all greater than 0");
         require(mapDCA[msg.sender][timestamp].periodDays == 0, "Error: Retry in a few seconds");
-
+        require(_fee5Decimals < 100000, "Error: Fee must be inferior than 1, with 5 decimals");
         //Register the DCA for this user
+        uint ownerFee = (_totalOccurences * _amountPerOccurrence * 100000) * 500 / 10000000000;
+        _amountPerOccurrence = _amountPerOccurrence - ownerFee / _totalOccurences;
         mapDCA[msg.sender][timestamp] = userDCAData(_period, _totalOccurences, 0, _amountPerOccurrence, _tokenIn, _tokenOut,
-                                                    _totalOccurences * _amountPerOccurrence, buildSwapPath(_tokenIn, _tokenOut), _fee10Decimals);
+                                                    _totalOccurences * _amountPerOccurrence, buildSwapPath(_tokenIn, _tokenOut), _fee5Decimals);
 
         if (_tokenIn != address(0)) {
             //Require the user to approve the transfer beforehand
@@ -234,10 +249,12 @@ contract DCApp{
         else {
             require(msg.value >= _totalOccurences * _amountPerOccurrence, "Error: You did not sent enough BNB");
         }
-
-        emit addedNewDCA(timestamp, msg.sender);
-
+        sendFee(owner, _tokenIn, ownerFee);
+        emit AddedNewDCA(timestamp, msg.sender);
+        
         executeSingleUserDCA(msg.sender, timestamp);
+
+        return timestamp;
     }
 
     function deleteUserDCA(uint _startDate) external {
@@ -251,6 +268,8 @@ contract DCApp{
 
         userDCAData memory userDCA = mapDCA[msg.sender][_startDate];
         require(userDCA.periodDays > 0, "Error: This DCA does not exist");
+        
+        emit DeletedDCA(_startDate, msg.sender);
         delete mapDCA[msg.sender][_startDate];
 
         if (userDCA.tokenIn != address(0)) {
@@ -261,15 +280,15 @@ contract DCApp{
             require(success, "BNB Transfer failed.");
         }
 
-        emit deletedDCA(_startDate, msg.sender);
+        
     }
 
-    function executeSingleUserDCA(address user, uint DCAStartDate ) public {
+    function executeSingleUserDCA(address _userAddress, uint _DCAStartDate ) public {
 
         /*  Execute a single occurence of a single DCA for the user
-            Find the right one by the user address and startDate value, which should be unique for each DCA of this user
+            Find the right one by the user address and startDate value, which is unique for each DCA of a specific user
             It implies that we must know this exact value i.e. querying it after creating the DCA, and stocking it on our servers
-            This function should be call by our server every automatically, depending on the period of the DCA
+            This function reward the caller with a percentage of the input token traded in an occurence, to compensate for the gas fees
             Steps :
              - Security checks
                - The DCA must exist
@@ -279,31 +298,42 @@ contract DCApp{
                 - Increment the number of occurence
                 - Modify the lockedAmount of each token
              - Swap the defined amount of tokens, minus the fee, with the swap() function 
+             - Send the fee to the caller of the function
              - Send the fee to the address that executed this function
              TODO: 
         */
-        userDCAData memory userDCA = mapDCA[user][DCAStartDate];
+        userDCAData memory userDCA = mapDCA[_userAddress][_DCAStartDate];
 
         require(userDCA.periodDays > 0, "Error: This DCA does not exist");
         require(userDCA.totalOccurences > userDCA.currentOccurence, "Error: DCA has already been completed, the user has to retrieve its tokens");
-        require(block.timestamp > DCAStartDate + userDCA.periodDays * /*86400*/ 1  * (userDCA.currentOccurence + 1) - 1800,
+        require(block.timestamp > _DCAStartDate + userDCA.periodDays * /*86400*/ 1  * (userDCA.currentOccurence + 1) - 1800,
                                                                                      "Error: Too soon to execute this DCA");
 
-        mapDCA[user][DCAStartDate].currentOccurence = userDCA.currentOccurence + 1;
+        mapDCA[_userAddress][_DCAStartDate].currentOccurence = userDCA.currentOccurence + 1;
+        uint feeCaller = 0;
+        if(msg.sender != _userAddress) {
+            feeCaller = (userDCA.amountPerOccurence * 100000) * userDCA.fee5Decimals / 10000000000;
+            sendFee(msg.sender, userDCA.tokenIn, feeCaller);
+        }
+        
+        uint amountWithoutFees = userDCA.amountPerOccurence - feeCaller;
+        uint estimatedMinimumAmountOut = IPancakeRouter02(exchangeRouterAddress)
+                            .getAmountsOut(amountWithoutFees, userDCA.swapPath)[userDCA.swapPath.length - 1];
 
-        uint amountOut = IPancakeRouter02(PANCAKEV2ROUTERTESTNET)
-                            .getAmountsOut(userDCA.amountPerOccurence, userDCA.swapPath)[userDCA.swapPath.length - 1];
+        mapDCA[_userAddress][_DCAStartDate].tokenInLockedAmount = userDCA.tokenInLockedAmount - userDCA.amountPerOccurence;
 
-        mapDCA[user][DCAStartDate].tokenInLockedAmount = userDCA.tokenInLockedAmount - userDCA.amountPerOccurence;
+        uint nextOccurrenceTimestamp = 0;
+        if(userDCA.totalOccurences > userDCA.currentOccurence + 1) {
+            nextOccurrenceTimestamp = block.timestamp + userDCA.periodDays * 86400;
+        }
+        emit OccurenceExecuted(_DCAStartDate, _userAddress, userDCA.totalOccurences, userDCA.currentOccurence + 1, block.timestamp, nextOccurrenceTimestamp, estimatedMinimumAmountOut);
 
-        uint amountWithoutFees = ((userDCA.amountPerOccurence * 100000) * (100000 - userDCA.fee5Decimals)) / 10000000000;
-        swap(userDCA.tokenIn, userDCA.tokenOut, userDCA.swapPath, amountWithoutFees, amountOut, user);
+        swap(userDCA.tokenIn, userDCA.tokenOut, userDCA.swapPath, amountWithoutFees, estimatedMinimumAmountOut, _userAddress);
 
-        IERC20(userDCA.tokenIn).transfer(msg.sender, userDCA.amountPerOccurence - amountWithoutFees);
-        emit occurenceExecuted(DCAStartDate, user, userDCA.totalOccurences, userDCA.currentOccurence + 1);
+        
     }   
 
-    function swap(address _tokenIn, address _tokenOut, address[] memory path, uint256 _amountIn, uint256 _amountOutMin, address userAddress) private {
+    function swap(address _tokenIn, address _tokenOut, address[] memory _path, uint256 _amountIn, uint256 _amountOutMin, address _userAddress) private {
         /*
             Swap _tokenIn for _tokenOut using the PancakeSwap RouterV2 contract
             If _tokenIn is an ERC20 it first gives _amountIn allowance to the router to do the swap
@@ -313,20 +343,20 @@ contract DCApp{
 
         //Case 1: Both tokens are ERC20 tokens
         if (_tokenIn != address(0) && _tokenOut != address(0)) {
-            IERC20(_tokenIn).approve(PANCAKEV2ROUTERTESTNET, _amountIn);
-            IPancakeRouter02(PANCAKEV2ROUTERTESTNET).swapExactTokensForTokensSupportingFeeOnTransferTokens
-                                                        (_amountIn, _amountOutMin, path, userAddress, block.timestamp);
+            IERC20(_tokenIn).approve(exchangeRouterAddress, _amountIn);
+            IPancakeRouter02(exchangeRouterAddress).swapExactTokensForTokensSupportingFeeOnTransferTokens
+                                                        (_amountIn, _amountOutMin, _path, _userAddress, block.timestamp);
         }
         //Case 2: _tokenIn is BNB
         else if (_tokenIn == address(0)) {
-            IPancakeRouter02(PANCAKEV2ROUTERTESTNET).swapExactETHForTokensSupportingFeeOnTransferTokens{value: _amountIn}
-                                                        (_amountOutMin, path,  userAddress, block.timestamp);
+            IPancakeRouter02(exchangeRouterAddress).swapExactETHForTokensSupportingFeeOnTransferTokens{value: _amountIn}
+                                                        (_amountOutMin, _path,  _userAddress, block.timestamp);
         }
         //Case 3: _tokenOut is BNB
         else if (_tokenOut == address(0)) {
-            IERC20(_tokenIn).approve(PANCAKEV2ROUTERTESTNET, _amountIn);
-            IPancakeRouter02(PANCAKEV2ROUTERTESTNET).swapExactTokensForETHSupportingFeeOnTransferTokens
-                                                        (_amountIn, _amountOutMin, path,  userAddress, block.timestamp);
+            IERC20(_tokenIn).approve(exchangeRouterAddress, _amountIn);
+            IPancakeRouter02(exchangeRouterAddress).swapExactTokensForETHSupportingFeeOnTransferTokens
+                                                        (_amountIn, _amountOutMin, _path,  _userAddress, block.timestamp);
         }
     }
 
@@ -344,29 +374,70 @@ contract DCApp{
         //Case 1 : The IN token is BNB
         if (_tokenIn == address(0)) {
             path = new address[](2);
-            path[0] = WBNBTestNet;
+            path[0] = WBNBTESTNET;
             path[1] = _tokenOut;
         }
         //Case 2 : The OUT token is BNB
         else if (_tokenOut == address(0)) {
             path = new address[](2);
             path[0] = _tokenIn;
-            path[1] = WBNBTestNet;
+            path[1] = WBNBTESTNET;
         }
         //Case 3 : Both token are ERC20 but swapping throught WBNB is possible
-        else if (_tokenIn == WBNBTestNet || _tokenOut == WBNBTestNet) {
+        else if (_tokenIn == WBNBTESTNET || _tokenOut == WBNBTESTNET) {
             path = new address[](2);
             path[0] = _tokenIn;
             path[1] = _tokenOut;
         } else {
             path = new address[](3);
             path[0] = _tokenIn;
-            path[1] = WBNBTestNet;
+            path[1] = WBNBTESTNET;
             path[2] = _tokenOut;
         }          
         
-
-
         return path;
+    }
+
+    function sendFee(address _recipient, address _tokenFee, uint _feeAmount) private {
+        if(_tokenFee != address(0)) {
+            IERC20(_tokenFee).transfer(_recipient, _feeAmount);
+        }
+        else {
+            (bool success, ) = _recipient.call{value: _feeAmount}("");
+            require(success, "BNB Transfer failed.");
+        }
+    }
+
+    function modifyDCAFee (uint _newFee5Decimals, uint _DCAStartDate) public {
+        /*
+            Allow user to change the fee for its DCA
+        */
+        require(_newFee5Decimals < 100000, "Error: Fee must be inferior than 1, with 5 decimals");
+        mapDCA[msg.sender][_DCAStartDate].fee5Decimals = _newFee5Decimals;
+    }
+
+    //Functions to get/set contract global variables
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+
+    function getExchangeRouterAddress() public view returns (address) {
+        return exchangeRouterAddress;
+    }
+
+    function getExchangeFactoryAddress() public view returns (address) {
+        return exchangeFactoryAddress;
+    }
+
+    function setOwner (address _newOwner) onlyOwner public {
+        owner = _newOwner;
+    }
+
+    function setExchangeRouterAddress (address _newExchangeRouterAddress) onlyOwner public {
+        exchangeRouterAddress = _newExchangeRouterAddress;
+    }
+
+    function setExchangeFactoryAddress (address _newExchangeFactoryAddress) onlyOwner public {
+        exchangeFactoryAddress = _newExchangeFactoryAddress;
     }
 }
