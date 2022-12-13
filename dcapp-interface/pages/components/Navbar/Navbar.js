ef6309af805dc417dbb20ce1c4bb8d46ccb7a976
n3wil <29715535+n3wil@users.noreply.github.com>
2022-12-13 17:41:28 +0100

    new interface
    nextjs interface creation

---
+++ dcapp-interface/pages/components/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { Grid } from "@mui/material";
+import TimelineIcon from '@mui/icons-material/Timeline';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import PriceCheckIcon from '@mui/icons-material/PriceCheck';
+import ConnectWalletButton from './ConnectWalletButton';
+import Tab from '@mui/material/Tab';
+import TabList from '@mui/lab/TabList';
+
+function Navbar(props){
+
+    const handleChange = (event, newValue) => {
+        props.setTab(newValue);
+    };
+
+    return(
+        <>
+            <Grid 
+              container 
+              alignItems="center"
+              direction={{xs: "column", md: "row"}}
+            >
+                <Grid item md={9}>
+                    <img src="/images/logo.png" className="logo" />
+                </Grid>
+
+                <Grid item md={1} >
+                    <ConnectWalletButton />
+                </Grid>
+            </Grid>
+            <Grid 
+              container 
+              alignItems="center"
+            >
+                <Grid item xs={12}>
+                    <TabList onChange={handleChange} aria-label="nav bar" centered >
+                        <Tab icon={<TimelineIcon />} label="New DCA" value="1" />
+                        <Tab icon={<FormatListBulletedIcon />} label="My DCA(s)" value="2" />
+                        <Tab icon={<PriceCheckIcon />} label="Pool" value="3" />
+                    </TabList>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default Navbar;
---
+++ dcapp-interface/pages/index.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+import Header from './components/Header';
+import App from './components/App';
+
+export default function Home() {
+
+  return (
+    <>
+      <Header />
+      <App />
+    </>
+  )
+}
