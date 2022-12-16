29a85e024c01793aa59f32023e728c9713544676
François Liard <56561415+Pacdac@users.noreply.github.com>
2022-12-16 17:33:50 +0100

    Fix bug user address and delete header

--- dcapp-website/src/App.js
+++ dcapp-website/src/App.js
@@ -3,69 +3,82 @@ import Navbar from './Components/Navbar/Navbar';
 import NewDCATab from './Components/Body/Tabs/NewDCATab/NewDCATab';
 import TabContext from '@mui/lab/TabContext';
 import TabPanel from '@mui/lab/TabPanel';
-import Headers from './Components/Header';
 import { UserContext } from './Context/UserContext';
 import { useEffect, useState, useContext } from 'react';
-import {ethers } from 'ethers';
+import { ethers } from 'ethers';
+import PoolTab from './Components/Body/Tabs/PoolTab/PoolTab';
 
 function App() {
 
-    const [value, setValue] = useState('1');
-    const { user, setUser } = useContext(UserContext);
-    const [connected, setConnected] = useState(false);
+  const [value, setValue] = useState('1');
+  const { user, setUser } = useContext(UserContext);
+  const [connected, setConnected] = useState(false);
 
-    async function connectWallet() {
-      if (window.ethereum) {
-        window.ethereum.request({ method: 'eth_requestAccounts' });
-      } else {
-        console.log("Non-Ethereum browser detected. You should consider trying MetaMask!");
-        return false;
-      }
+  async function connectWallet() {
+    if (window.ethereum) {
+      window.ethereum.request({ method: 'eth_requestAccounts' });
+    } else {
+      console.log("Non-Ethereum browser detected. You should consider trying MetaMask!");
+      return false;
     }
-  
-    async function isMetaMaskConnected() {
-      const { ethereum } = window;
-      const accounts = await ethereum.request({ method: 'eth_accounts' });
-      return accounts.length > 0;
+  }
+
+  async function isMetaMaskConnected() {
+    const { ethereum } = window;
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    return accounts.length > 0;
+  }
+
+  async function initialise() {
+    setConnected(await isMetaMaskConnected());
+    if (!connected) {
+      connectWallet();
     }
-  
-    async function initialise() {
-      setConnected(await isMetaMaskConnected());
-      if (!connected) {
-        connectWallet();
-      }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = await provider.getSigner();
+    setUser(await signer.getAddress());
+    console.log("apptest", user)
+  }
+
+  async function checkUserAddress() {
+    if (connected) {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = await provider.getSigner();
-      setUser(await signer.getAddress());
-    }
-  
-    useEffect(() => {
-      window.ethereum.on('accountsChanged', initialise);
-      return () => {
-        window.ethereum.removeListener('accountsChanged', initialise);
+      const userAddress = await signer.getAddress();
+      if (userAddress && userAddress !== user) {
+        setUser(userAddress);
       }
-    });
+    }
+  }
 
-    return (
-        <>
-            <Headers />
-            <TabContext value={value}>
-                <Navbar 
-                    setTab={setValue}
-                />
-                <TabPanel value="1">
-                    <NewDCATab />
-                </TabPanel>
+  useEffect(() => {
+    checkUserAddress();
+    window.ethereum.on('accountsChanged', initialise);
+    return () => {
+      window.ethereum.removeListener('accountsChanged', initialise);
+    }
+  });
 
-                <TabPanel value="2">
-                </TabPanel>
+  return (
+    <>
+      <TabContext value={value}>
+        <Navbar
+          setTab={setValue}
+        />
+        <TabPanel value="1">
+          <NewDCATab />
+        </TabPanel>
 
-                <TabPanel value="3">
-                </TabPanel>
+        <TabPanel value="2">
+        </TabPanel>
 
-            </TabContext>
-        </>
-    )
+        <TabPanel value="3">
+          <PoolTab />
+        </TabPanel>
+
+      </TabContext>
+    </>
+  )
 }
 
 export default App;
